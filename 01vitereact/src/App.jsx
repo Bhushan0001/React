296ad4d70aa12fe21ac255000965bc36a52a582d
Bhushan <bhushanharpadeo8@gmail.com>
2024-07-15 00:15:02 +0530

    first react project

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,13 @@
+import Chai from './chai'
+function App() {
+  const username = "chai aur code"
+  return (
+    <div>
+    <Chai />
+    <h3>chai aur react {username} </h3>
+    <p>test para</p>
+    </div>
+  )
+}
+
+export default App
